87e596329af393b3604d1c3aed97627d0dbf085b
Andy Thai <andy.thai9@gmail.com>
2017-01-24 13:04:49 -0800

    Cleaned up code

--- FrameGrabber/FrameGrabber.cpp
+++ FrameGrabber/FrameGrabber.cpp
@@ -11,50 +11,83 @@ const string AIR_ACTIVE = "Air Active";
 const string AIR_PASSIVE = "Air Passive";
 const string INTERIM = "Interim";
 const bool DEBUG = true;
+const string TEST_MP4 = "input/CSD2122sub-111615.mp4";
+const string TEST_CSV = "input/CSD2122sub-111615.csv";
 
 // Variables
-int num_intervals = 0; // Number of intervals read from .csv file
-vector<Interval> intervals;
+vector<Interval> intervals; // Container for intervals
+bool csv_load_first_try = true; // Prevents premature closing
 
+// Forward declarations
+static void load_video();
+static void load_csv();
+
+// TODO: Main method
 int main()
 {
 	cout << "Starting FrameGrabber program..." << endl;
 
+	// Load files
+	load_video();
+	load_csv();
+
+	// Exit behavior
+	cout << "Press ENTER to exit program." << endl;
+	cin.ignore();
+	if (!csv_load_first_try) {
+		cin.ignore();
+	}
+    return 0;
+}
+
+// TODO: Loads mp4 file
+static void load_video()
+{
 	// Get video file location
 	cout << "Input video file: ";
 	std::string video_filename;
 	getline(cin, video_filename);
-	std::cout << std::endl;
-	// Default test file
+
+	// Default test file if none specified
 	if (video_filename == "") {
-		video_filename = "input/CSD2122sub-111615.mp4";
+		video_filename = TEST_MP4;
 	}
 
+	// TODO: Loading video
+	cout << "Loading " << video_filename << "..." << endl << endl;
+}
+
+// Loads csv file
+static void load_csv()
+{
 	// Get .csv file location
 	cout << "Input .csv file: ";
 	std::string csv_filename;
 	getline(cin, csv_filename);
-	std::cout << std::endl;
-	// Default test file
+
+	// Default test file if none specified
 	if (csv_filename == "") {
-		csv_filename = "input/CSD2122sub-111615.csv";
+		csv_filename = TEST_CSV;
 	}
 
-	// TODO: Loading video
-	cout << "Loading " << video_filename << "..." << endl;
-
 	// Loading csv
-	cout << "Loading " << csv_filename << "..." << endl;
 	ifstream in_stream(csv_filename, ios::binary);
 
 	// Check if stream opens a valid file
-	if (in_stream.fail())
+	while (in_stream.fail())
 	{
-		cout << "Could not open file to read.\n"; // if the open file fails.
-		cout << "Press ENTER to exit the program." << endl;
-		cin.ignore();
-		return 1;
+		csv_load_first_try = false;
+		cout << "Could not open file " << csv_filename << " to read.\n"; // if the open file fails.
+		cout << "Please re-enter the csv file name: ";
+		getline(cin, csv_filename);
+		if (csv_filename == "") {
+			csv_filename = TEST_CSV;
+		}
+		in_stream.close();
+		in_stream.open(csv_filename, ios::binary);
 	}
+
+	cout << "Loading " << csv_filename << "..." << endl;
 
 	// Skip first line
 	string line;
@@ -62,54 +95,31 @@ int main()
 
 	// Read rest of .csv
 	while (getline(in_stream, line)) {
-		// Look for the three keywords
-		if (line.find(AIR_ACTIVE) != string::npos || 
-			line.find(AIR_PASSIVE) != string::npos || 
-			line.find(INTERIM) != string::npos) {
 
-			num_intervals++;
+		// Look for the three keywords
+		if (line.find(AIR_ACTIVE) != string::npos ||
+			line.find(AIR_PASSIVE) != string::npos ||
+			line.find(INTERIM) != string::npos) {
 
 			// Find start time
 			string delimiter = ",";
 			int start_find = line.find(delimiter);
 			string start_time = line.substr(0, start_find);
-			if (DEBUG) {
-				cout << start_time << endl;
-			}
 
 			// Find end time
 			string delimiter2 = ",,,";
 			int end_find = line.find(delimiter2);
 			string end_time = line.substr(start_find + 1, end_find - 8);
-			if (DEBUG) {
-				cout << end_time << endl;
-			}
 
 			// Find label / name
 			string name = line.substr(end_find + 3, line.length() - 1);
-			if (DEBUG) {
-				cout << name << endl;
-			}
 
 			// Create interval
 			Interval current_interval = Interval(name, start_time, end_time);
 			intervals.push_back(current_interval);
-
-			if (DEBUG) {
-				cout << line << endl << endl;
-			}
-		}
-	}
-	cout << "Number of valid intervals: " << num_intervals << endl;
-
-	if (DEBUG) {
-		for (int i = 0; i < intervals.size(); i++) {
-			cout << intervals[i].getName() << endl;
 		}
 	}
 
+	cout << "Amount of valid csv intervals: " << intervals.size() << endl << endl;
 	in_stream.close();
-	cin.ignore();
-    return 0;
 }
-
